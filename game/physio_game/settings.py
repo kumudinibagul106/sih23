f6432621a7599fc766e475e366f62ee817228eaf
kumudini <kumudinibagul106@gmail.com>
2023-12-20 15:19:04 +0530

    done vercel setting

--- game/physio_game/settings.py
+++ game/physio_game/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)a0=k%s-e8jd(qs32tx#z$^&iam0kbtnyx4qcr&nwek=mj0#b&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'static')
